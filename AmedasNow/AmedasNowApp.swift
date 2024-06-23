70a11e63b4c8c4887b63eaaa83751af0f66fb8d9
karifol <karigolhorinouchi@gmail.com>
2024-06-23 17:55:32 +0900

    Initial Commit

---
+++ AmedasNow/AmedasNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmedasNowApp.swift
+//  AmedasNow
+//
+//  Created by 堀ノ内海斗 on 2024/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AmedasNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
