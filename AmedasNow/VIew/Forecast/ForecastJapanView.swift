1b5ee21d2635cffa08d1672afbbe008616f351ef
karifol <karigolhorinouchi@gmail.com>
2024-08-02 21:03:35 +0900

    はんら

--- AmedasNow/VIew/Forecast/ForecastJapanView.swift
+++ AmedasNow/VIew/Forecast/ForecastJapanView.swift
@@ -3,21 +3,48 @@ import MapKit
 
 struct ForecastJapanView: View {
     @State private var cameraPosition = MapCameraPosition.region(MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 35.711, longitude: 139.866),
-        span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)
+        center: CLLocationCoordinate2D(latitude: 35, longitude: 137),
+        span: MKCoordinateSpan(latitudeDelta: 20, longitudeDelta: 20)
     ))
     var body: some View {
-        Map(position: $cameraPosition){
-            let targetCoordinate = CLLocationCoordinate2D(
-                latitude: 36.69,
-                longitude: 138.93
-            )
-            Annotation("Annotation", coordinate: targetCoordinate, anchor: .center) {
-                    Text("test")
-                    .background(.red)
-                    .frame(width: 300, height: 200)
+        VStack(spacing: 0){
+            HeaderView
+            Map(position: $cameraPosition){
+                let targetCoordinate = CLLocationCoordinate2D(
+                    latitude: 36.69,
+                    longitude: 138.93
+                )
+                Annotation("東京", coordinate: targetCoordinate, anchor: .center) {
+                    Button{
+                    
+                    } label: {
+                        Image("101")
+                            .resizable()
+                            .frame(width: 60, height: 50)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    }
+                }
             }
+            .mapStyle(.imagery(elevation: .realistic))
         }
+
+    }
+}
+
+extension ForecastJapanView {
+    // header
+    private var HeaderView: some View {
+        HStack {
+            Image(systemName: "cloud.sun.rain")
+            Text("天気予報")
+        }
+        .foregroundStyle(.white)
+        .font(.title2)
+        .frame(maxWidth: .infinity)
+        .background(.blue)
+        .fontWeight(.bold)
     }
 }
 
